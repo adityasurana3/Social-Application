8691fb72f43a5e32580e7df28108b288819d8bed
Aditya Surana <aditya.surana@proshore.eu>
2023-11-30 19:45:55 +0545

    action app created

--- account/views.py
+++ account/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 
 # Create your views here.
 # def user_login(request):
@@ -33,7 +35,12 @@ from .models import Contact
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html',{'section':'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.followwing.value_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
+    return render(request, 'account/dashboard.html',{'section':'dashboard','actions':actions})
 
 def register(request):
     if request.method == 'POST':
@@ -42,6 +49,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            create_action(new_user, 'has created an account')
             # Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html',{'new_user':new_user})
     else:
@@ -83,6 +91,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is_following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status":"ok"})
